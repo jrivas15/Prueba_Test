7dd866da9bcc19047d759d1b663c19e97d3002e1
jrivas15 <85762699+jrivas15@users.noreply.github.com>
2021-06-11 19:13:47 -0500

    version 2

--- test.py
+++ test.py
@@ -8,7 +8,6 @@ G = {"a": ["c"],
 
 if __name__ == "__main__":
     keys = list(G.keys())
-    values = list(G.values())
     for i in keys:
         print(f"Entrada: {i} y G Salida: {len(G[i])} ")
 
